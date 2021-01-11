7e46cc7feafd65e060ed3266d42e11c9e27c5896
GreenBudgie <forestengine@yandex.ru>
2021-01-11 18:04:44 +0300

    Keeping notes inside container

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -6,13 +6,17 @@ const defaultWidth = 300;
 const defaultHeight = 500;
 
 export default function Note({maxX, maxY}) {
-  const [x, setX] = useState(Math.floor(Math.random() * (maxX - defaultWidth)));
-  const [y, setY] = useState(Math.floor(Math.random() * (maxY - defaultHeight)));
+  const [width, setWidth] = useState(defaultWidth);
+  const [height, setHeight] = useState(defaultHeight);
+  const [x, setX] = useState(Math.floor(Math.random() * (maxX - width)));
+  const [y, setY] = useState(Math.floor(Math.random() * (maxY - height)));
   const [movementStatus, setMovementStatus] = useState({moving: false, startX: null, startY: null, pivotX: null, pivotY: null});
 
+  //Sets the current position relative to the NoteHandler. Prevents the note from going outside the boundaries.
   function setPosition(x, y) {
-    setX(x);
-    setY(y);
+    const clamp0 = (n, max) => n < 0 ? 0 : (n > max ? max : n);
+    setX(clamp0(x, maxX - width));
+    setY(clamp0(y, maxY - height));
   }
 
   function startMoving(event) {
