a63e9401dcb00464c33f634b3eaeb2c3a151aa1a
GreenBudgie <forestengine@yandex.ru>
2021-01-12 19:51:31 +0300

    Visual movement improvements

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from 'react';
 import { ResizableBox } from 'react-resizable'
-import sizeMe from 'react-sizeme';
 
 const defaultWidth = 300;
 const defaultHeight = 500;
@@ -8,22 +7,25 @@ const defaultHeight = 500;
 function Note({maxX, maxY, cursor}) {
   const [x, setX] = useState(Math.floor(Math.random() * (maxX - defaultWidth)));
   const [y, setY] = useState(Math.floor(Math.random() * (maxY - defaultHeight)));
-  
+  const [moveStyle, setMoveStyle] = useState({scale: 1, opacity: 1});
+
   const movementStatus = useRef({moving: false, startX: null, startY: null, pivotX: null, pivotY: null});
   const intervalId = useRef(null);
-  const ref = useRef(null);
+  const boxRef = useRef(null);
+  const noteRef = useRef(null);
 
   //Sets the current position relative to the NoteHandler. Prevents the note from going outside the boundaries.
   function setPosition(x, y) {
     const clamp0 = (n, max) => n < 0 ? 0 : (n > max ? max : n);
-    setX(clamp0(x, maxX - ref.current.state.width));
-    setY(clamp0(y, maxY - ref.current.state.height));
+    setX(clamp0(x, maxX - boxRef.current.state.width));
+    setY(clamp0(y, maxY - boxRef.current.state.height));
   }
 
   function startMoving(event) {
     if(event.target.className === "note-wrapper" && !movementStatus.current.moving) {
       movementStatus.current = {moving: true, startX: x, startY: y, pivotX: event.clientX, pivotY: event.clientY};
       cursor.current.forceStop = false;
+      setMoveStyle({scale: 0.98, opacity: 0.9});
       intervalId.current = setInterval(() => {
         handleMovement();
       }, 20);
@@ -41,8 +43,9 @@ function Note({maxX, maxY, cursor}) {
   function stopMoving() {
     if(movementStatus.current.moving) {
       movementStatus.current.moving = false;
-      if(intervalId != null) {
-        clearInterval(intervalId);
+      setMoveStyle({scale: 1, opacity: 1});
+      if(intervalId.current != null) {
+        clearInterval(intervalId.current);
       }
     } 
   }
@@ -54,14 +57,19 @@ function Note({maxX, maxY, cursor}) {
       height={defaultHeight}
       minConstraints={[240, 200]}
       maxConstraints={[maxX - x, maxY - y]}
-      ref={ref}
+      ref={boxRef}
       style={{
-        transform: 'translate(' + x + 'px, ' + y + 'px)'
+        transform: 'translate(' + x + 'px, ' + y + 'px)',
       }}
     >
       <div 
         className="note-wrapper"
+        ref={noteRef}
         onMouseDown={startMoving}
+        style={{
+          transform: 'scale(' + moveStyle.scale + ')',
+          opacity: moveStyle.opacity
+        }}
       >
         <input type="text"/>
         <textarea></textarea>
