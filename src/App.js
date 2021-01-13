2ae212343165a5325f9727690decf0727364eb60
GreenBudgie <forestengine@yandex.ru>
2021-01-13 15:50:14 +0300

    Global cursor position handling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import NoteHandler, {NoteProvider} from './components/Notes/NoteHandler'
 
 function App() {
+  const globalCursorPosition = React.useRef({x: null, y: null, forceStop: false});
+  
   return (
     <NoteProvider>
-      <div className="main-wrapper">
+      <div 
+        className="main-wrapper"
+        onMouseMove={event => {globalCursorPosition.current.x = event.clientX; globalCursorPosition.current.y = event.clientY}}
+        onMouseLeave={() => globalCursorPosition.current.forceStop = true}
+        onMouseUp={() => globalCursorPosition.current.forceStop = true}
+      >
         <Sidebar />
-        <NoteHandler />
+        <NoteHandler cursor={globalCursorPosition}/>
       </div>
     </NoteProvider>
   );
